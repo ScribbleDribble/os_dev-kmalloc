c7e7b48f78a1fc404bca7380ccc7dcbcd0f7c8e6
Micah <m1cah@hotmail.co.uk>
2023-03-04 19:01:53 +0000

    add coalescing for freed blocks + fixes for pointer code

--- local_vmm.h
+++ local_vmm.h
@@ -1,6 +1,7 @@
 // #include <stdio.h> 
 #include <stdbool.h>
 #include <stdint.h>
+#include <string.h>
 #include <stdio.h>
 #include <sys/mman.h>
 
@@ -8,18 +9,18 @@
 
     Block:
 
-    -----------------------------
+    ----------------------------- 0x50000
     front block header (4 bytes)                    
 
-    -----------------------------
+    ----------------------------- 0x50004
 
             payload 
 
-    -----------------------------
+    ----------------------------- 0x5000C
     
-    padding (to make sure Block is 8-bit aligned in memory)
+    optional padding (to make sure Block is 8-bit aligned in memory)
 
-    ------------------------------
+    ------------------------------0x5000C
 
      back block header (4 bytes)
     
@@ -44,9 +45,16 @@ typedef struct block_header_t {
 #define IS_FITTING(block_header, requested_size) (requested_size > block_header->size - 2 * sizeof(block_header))
 #define IS_VALID_BLOCK(block_header, requested_size) (!IS_ALLOCATED(block_header) && IS_FITTING(block_header, requested_size))
 
+#define JMP_TO_NEXT_BH(bh) ( (block_header_t*) ( (void*) bh + GET_PAYLOAD_SIZE(bh) + BLOCK_HEADER_SIZE) )
+
+#define GET_PAYLOAD_SIZE(block_header) (block_header->size - BLOCK_HEADER_SIZE * 2)
 #define SET_ALLOCATED(block_header) (block_header->status = ALLOCATED)
+#define SET_FREE(block_header) (block_header->status = FREE)
 
 block_header_t* head;
+block_header_t* tail;
+
+static uint32_t allocs = 0;
 
 static block_header_t create_block_header(uint16_t size, bool is_allocated) {
     block_header_t bh;
@@ -55,22 +63,131 @@ static block_header_t create_block_header(uint16_t size, bool is_allocated) {
     return bh;
 }
 
+
+void allocate_block(block_header_t* bh) {
+    SET_ALLOCATED(bh);
+    SET_ALLOCATED(JMP_TO_NEXT_BH(bh));
+}
+
+void free_block(block_header_t* bh) {
+    SET_FREE(bh);
+    printf("clearing %x\n", ((void*)bh+BLOCK_HEADER_SIZE));
+    memset((void*)bh+BLOCK_HEADER_SIZE, 0, GET_PAYLOAD_SIZE(bh));
+    SET_FREE(JMP_TO_NEXT_BH(bh));
+}
+
 void* first_fit(uint32_t size){
     void* ptr = (void*) head;
     block_header_t* bh_ptr = (block_header_t*) head;
+    int i = 0;
     while (bh_ptr->size != 0) {
         if (IS_VALID_BLOCK(bh_ptr, size)) {
-            SET_ALLOCATED(bh_ptr);
-            printf("Allocating memory at address 0x%x\n",(uint32_t) ((void*) ptr) + sizeof(block_header_t));
-            return ((void*) ptr) + sizeof(block_header_t); 
+            // SET_ALLOCATED(bh_ptr);
+            allocate_block(bh_ptr);
+            printf("Allocating memory at address 0x%x\nWithin block %u\nblock size of 0x%x bytes\n------------\n",
+            (uint32_t) ((void*) ptr) + BLOCK_HEADER_SIZE, i, bh_ptr->size);
+            allocs += 1;
+            return ((void*) ptr) + BLOCK_HEADER_SIZE; 
         }
+
         ptr +=  bh_ptr->size;
         bh_ptr = (block_header_t*) ptr;
-    } 
+        i++;
+    }
+    printf("Unable to allocate memory of size %u\n", size); 
 }
+
+// check address inputs for memset. 
 
 void* kmalloc(uint32_t size){
     return first_fit(size);
+}
+
+void coalesce(block_header_t* bh_ptr) {
+
+    int new_block_size = 0;
+
+    // if the current block that has been free has a prev or next block free, then merge
+    
+    //
+    // bounds checking
+    if (bh_ptr - 1 > head && (bh_ptr-1)->status == FREE) {
+        new_block_size = bh_ptr->size + (bh_ptr-1)->size;
+
+        bh_ptr -= 1;
+        void* ptr = bh_ptr;
+
+        
+        // jump to start of new block and clear old block headers
+        ptr -= BLOCK_HEADER_SIZE + GET_PAYLOAD_SIZE(bh_ptr);
+        memset(bh_ptr, 0, BLOCK_HEADER_SIZE*2);
+        bh_ptr = (block_header_t *)ptr;
+        *bh_ptr = create_block_header(new_block_size, FREE);
+        
+        ptr += new_block_size-BLOCK_HEADER_SIZE;
+        bh_ptr = ptr;
+
+        // set size of new block header end
+        bh_ptr->size = new_block_size;    
+
+        printf("coalesced above block\n");
+
+    }
+
+    // | bh_start | payload | bh_end | bh_start | ...
+//                          ^  
+//                         here
+
+    printf("cur %x | tail %x", bh_ptr, tail);
+    if (bh_ptr + 1 >= tail || (bh_ptr+1)->status != FREE) {
+        printf("no\n");
+        return;
+    }
+
+    // move ptr into similar position as above code.
+    bh_ptr += 1;
+
+    new_block_size = bh_ptr->size + (bh_ptr-1)->size;
+
+    bh_ptr -= 1;
+    void* ptr = bh_ptr;
+
+    // jump to start of new block and clear old block headers
+    ptr -= BLOCK_HEADER_SIZE + GET_PAYLOAD_SIZE(bh_ptr);
+    memset(bh_ptr, 0, BLOCK_HEADER_SIZE*2);
+
+    // we're now at bh_start of newly coalesced block
+    bh_ptr = (block_header_t *)ptr;
+    bh_ptr->size = new_block_size; 
+    bh_ptr->status = FREE;
+
+    ptr += new_block_size-BLOCK_HEADER_SIZE;
+    bh_ptr = ptr;
+
+    // set size of new block header end
+    bh_ptr->size = new_block_size; 
+
+    printf("coalesced below block\n");
+    // check if prev block free
+    // if free, 0 out the joining block headers, and then adjust the new bh front and end with the new infomation
+
+}
+
+
+void free(void* ptr) {
+
+    block_header_t* bh = (block_header_t*) (ptr - BLOCK_HEADER_SIZE);
+    printf("freeing pointer allocated at 0x%x\n", (uint32_t) ptr);
+    free_block(bh);
+
+    allocs -= 1;
+
+    if (allocs == 0) {
+        printf("freeing heap completely.\n");
+        munmap((void*)head, 0x1000);
+    }
+
+    coalesce(bh);
 }
 
 
@@ -81,14 +198,12 @@ void create_free_list(void* base_address) {
     
     head = (block_header_t*) base_address;
     void* ptr = base_address;
-    printf("%x\n", ((uint32_t) ptr));
-
     for (i = 0; i < end-1; i++) {
         block_header_t bh_front = create_block_header(DEFAULT_BLOCK_SIZE_BYTES, FREE);
         *(block_header_t*)ptr = bh_front;
         // advance to where the back block header should be
         ptr += DEFAULT_PAYLOAD_SIZE_BYTES + sizeof(block_header_t);
-        // printf("%u\n", (uint32_t) ptr);
+
         block_header_t bh_back = create_block_header(DEFAULT_BLOCK_SIZE_BYTES, FREE);
         *(block_header_t*)ptr = bh_back;
 
@@ -96,11 +211,14 @@ void create_free_list(void* base_address) {
         ptr += sizeof(block_header_t);
         
     }
-    block_header_t list_tail_bh = create_block_header(0, true);
-    *(block_header_t*)ptr = list_tail_bh;
+    tail = ptr;
+    *tail = create_block_header(0, true);
 
 }
     
+
+void free_list_status_printer(int from, int to) {
+}
 
 void init_heap() {
 
--- main.c
+++ main.c
@@ -1,6 +1,5 @@
-#include "local_vmm.h"
 #include <stdio.h>
-
+#include "local_vmm.h"
 
 
 int main(void) {
@@ -8,8 +7,14 @@ int main(void) {
     init_heap();
 
     int x = 5;
-    char* s = (char*) kmalloc(x*sizeof(char));
+    char* s1 = (char*) kmalloc(x*sizeof(char));
+    // char* s2 = (char*) kmalloc(50*sizeof(char));
 
+    char* s3 = (char*) kmalloc(x*sizeof(char));
+
+    char* s4 = (char*)kmalloc(x*sizeof(char));
+
+    free(s4);
     // s = "hey\0";
     // printf("%i", *get_num2());
     // int* b_ptr = (int*)kmalloc(sizeof(a));
